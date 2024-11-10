4bc2855b95b25c256ca8b72ec2996344a8dbad4a
imUmer <imumerniaz@gmail.com>
2024-11-10 23:32:12 +0300

    work on js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 let projectsContainer = document.getElementsByClassName("projects-container");
 let title = document.getElementsByClassName("title");
 
-function test () {
-
+function test (name) {
+    console.log(name);
+    
 }
 
 
+test('jen');
