db5f7f8bbc6b02c8e33a093c75bc188dcf3e36e9
imUmer <imumerniaz@gmail.com>
2024-11-14 23:47:46 +0300

    work on text shadow

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,19 @@ function test (name) {
 
 function onHoverTitle () {
     // how to change title . next day task
+    
 }
+
+const text = document.querySelector('.light-follow-text');
+
+  // Event listener to track mouse movement
+  document.addEventListener('mousemove', (event) => {
+    const { clientX, clientY } = event;
+
+    // Calculate shadow offset based on mouse position relative to window size
+    const xOffset = (window.innerWidth / 2 - clientX) / 30;
+    const yOffset = (window.innerHeight / 2 - clientY) / 30;
+
+    // Set the text-shadow based on calculated offsets
+    text.style.textShadow = `${xOffset}px ${yOffset}px 10px rgba(255, 255, 255, 0.7)`;
+  });
